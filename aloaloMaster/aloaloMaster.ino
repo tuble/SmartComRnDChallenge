2cb5a6f106c4bd2a836220c17128b9ac665d4dd4
tuble <iana.bratoeva@gmail.com>
2019-07-09 11:38:46 +0300

    wk2

--- aloaloMaster/aloaloMaster.ino
+++ aloaloMaster/aloaloMaster.ino
@@ -4,40 +4,53 @@
 
 #define MOSI 0
 #define CLOCK_OUT 2
+#define AUDIO_OUT 3
+
+#define MESSAGE_LENGTH 1
+#define NOTE_LENGTH 250
 
 // MOSI -> TP4 (0 TO 3) bqla jica
 // CLK -> CLK (2 TO 2) cherna jica
-// COMMON GND & VCC
+// COMMON GND & VCC ... only common GND is needed?
 
-int i = 0;
+bool msg_sent;
 
-int info[] = {1, 0, 1, 0};
+int info[MESSAGE_LENGTH] = {1, 0, 1, 0};
 
 void setup() {
-
   pinMode(MOSI, OUTPUT);
   pinMode(CLOCK_OUT, OUTPUT);
+  pinMode(AUDIO_OUT, OUTPUT);
   digitalWrite(CLOCK_OUT, HIGH);
-
+  msg_sent = false;
 }
 
 void loop() {
+  // send message
 
-  // def a signal send window
-
-  while (digitalRead(CLOCK_OUT)) {
-    send_4_bits();
-    digitalWrite(CLOCK_OUT, LOW);
+  while (digitalRead(CLOCK_OUT) == 1 && msg_sent == false) {
+    send_bits();
+    digitalWrite(CLOCK_OUT, LOW); // stop msg transmission
+    msg_sent = true;
   }
 
-
+  // play note sequence
+  play_note(1);
+  delay(NOTE_LENGTH);
+//  play_note(1);
+//  delay(NOTE_LENGTH);
 }
 
-void send_4_bits() {
-
-  for (int i = 0; i < 4; i++) {
+void send_bits() {
+  for (int i = 0; i < MESSAGE_LENGTH; i++) {
     digitalWrite(MOSI, info[i]);
     delay(250);
   }
+}
 
+void play_note(float half_period) {
+  digitalWrite(AUDIO_OUT, HIGH);
+  delay(half_period * 2);
+  digitalWrite(AUDIO_OUT, LOW);
+  delay(half_period * 2);
 }
